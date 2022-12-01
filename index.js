4341e828912040f06cd02b73b84d514c95692d60
mknyambura <kariuki.mercie@gmail.com>
2022-12-02 00:29:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((respond) => {
+      return respond.json()
+    })
+    .then((json) => {
+      return renderBooks(json)
+    })
 }
 
 function renderBooks(books) {
